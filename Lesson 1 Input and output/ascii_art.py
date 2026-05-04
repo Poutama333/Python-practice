77f4ed959ffa77676172e83cbb22dbb0cc90ae9e
Poutama333 <lintolareihanat@wsc.school.nz>
2026-05-05 11:16:57 +1200

    Create ascii_art.py

---
+++ Lesson 1 Input and output/ascii_art.py
@@ -0,0 +1,20 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+print("</\/\/\/\/\/\/\/\/\/>")
+print("|    _         _    |")
+print("|    o         o    |")
+print("|          <        |")
+print("|         (_)       |")
+print("|___________________|")
+
+
+
+
+
+
+
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
