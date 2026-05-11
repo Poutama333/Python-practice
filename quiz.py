92aa64b0a08653bb6f9dcd4b893f14b9dbe0276b
Poutama333 <lintolareihanat@wsc.school.nz>
2026-05-12 11:05:52 +1200

    Quiz question 1

---
+++ quiz.py
@@ -0,0 +1,31 @@
+# Quiz
+
+
+
+# Question
+question1 = input("Question 1: \n"
+" What is the capital city of New Zealand, is it  \n"
+" a) Russel  \n"
+" b) Christchurch \n"
+" c) Wellington  \n"
+" d) Auckland \n")
+
+if question1 == "c":
+    print("Correct!")
+elif question1 == "a":
+    print("Incorrect")
+elif question1 == "b":
+    print("Incorrect")
+elif question1 == "d":
+    print("Incorrect")
+
+question2 = input("Question 2: \n"
+" What is the , is it  \n"
+" a) Russel  \n"
+" b) Christchurch \n"
+" c) Wellington  \n"
+" d) Auckland \n")
+
+
+
+
