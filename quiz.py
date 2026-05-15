01c9f8ddc657476c10b4cb8f79d0cf30d8d48304
Poutama333 <lintolareihanat@wsc.school.nz>
2026-05-15 12:54:33 +1200

    pry much

--- quiz.py
+++ quiz.py
@@ -15,14 +15,44 @@ if question1 == "c":
 else:
     print("Incorrect")
 
-
+print("\n" "\n" "\n")
 question2 = input("Question 2: \n"
-" What is the , is it  \n"
-" a) Russel  \n"
-" b) Christchurch \n"
-" c) Wellington  \n"
-" d) Auckland \n")
+" What is the fastest animal on earth, is it  \n"
+" a) Cheetah  \n"
+" b) Peregrine Falcon \n"
+" c) Marlin  \n"
+" d) Fly \n")
 
+if question1 == "b" or "B":
+    print("Correct")
+else:
+    print("Incorrect")
+print("\n" "\n" "\n")
+question3 = input("Question 3: \n"
+"What is the largest city \n"
+" in the world by landmass\n"
+"Is it:\n"
+"a) Auckland\n"
+"b) Tokyo\n"
+"c) Chongqing \n"
+"d) Vatican city\n")
 
+if question3 == "c" or "C":
+    print("Correct")
+else:
+    print("Incorrect")
+print("\n" "\n" "\n")
+question4 = input("Question 4: \n"
+" \n"
+" \n"
+"Is it:\n"
+"a)\n"
+"b)\n"
+"c)\n"
+"d)\n")
 
-
+if question4 == "":
+    print("Correct!")
+else:
+    print("Incorrect")
+print("\n" "\n" "\n")
