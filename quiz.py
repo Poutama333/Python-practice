9065dfb0fa176989be3f47db4c12080630101a84
Poutama333 <lintolareihanat@wsc.school.nz>
2026-05-17 13:28:09 +1200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -10,19 +10,48 @@ question1 = input("Question 1: \n"
 " c) Wellington  \n"
 " d) Auckland \n")
 
-if question1 == "c":
+if question1 == "c" or "":
     print("Correct!")
 else:
     print("Incorrect")
 
 
 question2 = input("Question 2: \n"
-" What is the , is it  \n"
-" a) Russel  \n"
-" b) Christchurch \n"
-" c) Wellington  \n"
-" d) Auckland \n")
+" What is the fastest animal on earth\n"
+" a)Cheetah\n"
+" b)Panther\n"
+" c)Peregrine falcon\n"
+" d)Pigeon\n")
+
+if question2 == "c" or "C":
+    print("Correct!")
+else:
+    print("Incorrect")
+print("\n")
 
 
+question3 = input("Question 3:\n"
+"What does KHCL stand for\n"
+" a)Keith Hunter central Learning\n"
+" b)Ken Havill Centre for Learning\n"
+" c)Kool Hlucks Clan Location\n"
+" d)Kiev Hiev Ciev Liev\n")
 
+if question3 == "b" or "B":
+    print("Correct!")
+else:
+    print("Incorrect")
+print("\n")
 
+question4 = input("Question 4:\n"
+"What \n"
+" a)\n"
+" b)\n"
+" c)\n"
+" d)\n")
+
+if question4 == "" or "":
+    print("Correct!")
+else:
+    print("Incorrect")
+print("\n")
